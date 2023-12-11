e3716c8f248746b5bc5bf7eb31374de7e98f3be0
eppsn8557 <145823339+eppsn8557@users.noreply.github.com>
2023-12-10 21:00:23 -0500

    P2LAB2_EppsNadia

---
+++ P2LAB2_EppsNadia.py
@@ -0,0 +1,9 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+product_all = num1 * num2 * num3 * num4
+sum_all = (num1 + num2 + num3 + num4) / 4
+print(f'{product_all:.0f} {sum_all:.0f}')
+print(f'{product_all:.3f} {sum_all:.3f}')
